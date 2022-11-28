e563101fb37ded78ba5cdb7d82ed6e2103cf1e85
velicanakcakaya <v.akcakaya@hanntech.de>
2022-11-28 18:49:29 +0100

    fix(resources): Fixed the `link_description` struct having the `message` field required.

--- src/resources/link_description.rs
+++ src/resources/link_description.rs
@@ -1,8 +1,6 @@
 use crate::resources::enums::http_method::HttpMethod;
 use serde::{Deserialize, Serialize};
-// use serde_with::skip_serializing_none;
 
-// #[skip_serializing_none]
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct LinkDescription {
     /// The complete target URL. To make the related call, combine the method with this URI Template-formatted link. For pre-processing,
@@ -12,6 +10,6 @@ pub struct LinkDescription {
     /// The link relation type, which serves as an ID for a link that unambiguously describes the semantics of the link. See Link Relations.
     pub rel: String,
 
-    /// The HTTP method required to make the related call.
-    pub method: HttpMethod,
+    /// The HTTP method. If present, use this method to make a request to the target URL. If absent, the default method is GET.
+    pub method: Option<HttpMethod>,
 }
